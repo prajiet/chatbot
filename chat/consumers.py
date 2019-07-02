d535569fc5dcac9a393b8a91ff5ad0eb98451514
Prajiet <prajiet.entrayn@gmail.com>
2019-07-03 04:32:16 +0530

    first

---
+++ chat/consumers.py
@@ -0,0 +1,105 @@
+from django.contrib.auth import get_user_model
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+import json
+from .models import Message, Chat, Contact
+from .views import get_last_10_messages, get_user_contact, get_current_chat
+import paralleldots
+
+User = get_user_model()
+
+
+class ChatConsumer(WebsocketConsumer):
+
+    def fetch_messages(self, data):
+        messages = get_last_10_messages(data['chatId'])
+        content = {
+            'command': 'messages',
+            'messages': self.messages_to_json(messages)
+        }
+        self.send_message(content)
+
+    def new_message(self, data):
+        user_contact = get_user_contact(data['from'])
+        message = Message.objects.create(
+            contact=user_contact,
+            content=data['message'])
+        current_chat = get_current_chat(data['chatId'])
+        current_chat.messages.add(message)
+        current_chat.save()
+        content = {
+            'command': 'new_message',
+            'message': self.message_to_json(message)
+        }
+        return self.send_chat_message(content)
+
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+        return result
+
+    def message_to_json(self, message):
+        paralleldots.set_api_key('h9LfrAJ0L32GNVC9zK4aw6K948YuUFm2oGAoBVPCfaU')
+        emotion = paralleldots.sentiment(message.content)['sentiment']
+        value=0
+        exp=''
+        for emotions,values in emotion.items():
+            if values > value:
+                value= values
+                exp=emotions
+        if exp=='positive':
+            emoji=':)'
+        elif exp=='negative':
+            emoji=='negative'
+        else:
+            emoji=':|'
+
+        return {
+
+            'id': message.id,
+            'author': message.contact.user.username,
+            'content': message.content,
+            'timestamp': str(message.timestamp),
+            'emoji': emoji
+        }
+
+    commands = {
+        'fetch_messages': fetch_messages,
+        'new_message': new_message
+    }
+
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        self.commands[data['command']](self, data)
+
+    def send_chat_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
+
+    def chat_message(self, event):
+        message = event['message']
+        self.send(text_data=json.dumps(message))
